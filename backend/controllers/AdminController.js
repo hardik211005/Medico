7252cf0bb4ff0f59b7edd9d46231a8d4ab7306fd
hardik211005 <hardikdhameeja2105@gmail.com>
2025-07-15 14:06:56 +0530

    Implement addDoctor API with validation and image upload functionality

---
+++ backend/controllers/AdminController.js
@@ -0,0 +1,62 @@
+import validator from "validator"
+import bcrypt from 'bcrypt'
+import { v2 } from "cloudinary"
+import doctorModel from "../models/DoctorModel.js"
+
+
+
+// API For adding doctor
+const addDoctor = async (req,res)=> {
+    try{
+
+        
+        const {name, email, password, speciality , degree, experience, about, fees , address} = req.body
+    const imageFile = req.file;
+
+    //checking for all data to add doctor
+    if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
+            return res.json({ success: false, message: "Missing Details" })
+        }
+         if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        }
+        // validating strong password
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
+        }
+         // hashing user password
+        const salt = await bcrypt.genSalt(10); // the more no. round the more time it will take
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" })
+        const imageUrl = imageUpload.secure_url
+
+        const doctordata = {
+            name,
+            email,
+            image:imageUrl,
+            password:hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+        const newDoctor = new doctorModel(docterData)
+        await newDoctor.save()
+
+        res.json({success:true,message:"Doctor Added"})
+ 
+
+    
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+
+    }
+}
+export {addDoctor}
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import 'dotenv/config'
 import connectDB from "./db/index.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminroute.js";
 
 const app = express();
 const port = process.env.port || 8000
@@ -25,6 +26,8 @@ app.use(cookieParser());
 //routes
 
 //api endpoints
+app.use('/api/admin',adminRouter)
+
 app.get('/',(req,res)=>{
   res.send("Api WOrking gr8")
 })
