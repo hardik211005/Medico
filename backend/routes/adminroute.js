9ae1eccfeb71e3527822812445193c681f0f4e4a
hardik211005 <hardik2105@gmail.com>
2025-07-18 16:00:17 +0530

    feat: initialize admin project with Vite and React setup
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application layout and components.
    - Developed App.jsx as the main application component with state management and interactivity.
    - Added React logo SVG in the assets directory.
    - Established global styles in index.css for consistent theming and layout.
    - Implemented main.jsx for rendering the application using React's StrictMode.
    - Configured Vite with a basic setup in vite.config.js for React support.

--- backend/routes/adminroute.js
+++ backend/routes/adminroute.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import { addDoctor } from '../controllers/adminController.js'
-import upload from '../middlewares/multer.js'
+import express from "express";
+import { addDoctor } from "../controllers/adminController.js";
+import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/authAdmin.js";
 
-const adminRouter = express.Router()
+const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor)
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 
-export default adminRouter
+export default adminRouter;
